26381d16a3349c09fb72c3d89a4014318398f31a
iceycc <w@icey.cc>
2017-11-22 16:16:27 +0800

    6-mall-page-fullage三四也完成 less注释乱码了 郁闷

--- mall-page-fullpage/js/index.js
+++ mall-page-fullpage/js/index.js
@@ -11,7 +11,29 @@ $(function () {
 			$(this).addClass("select");
 		},
 		//从当前屏进入到下一屏
-		onLeave: function (index, nextindex, direction) {},
+		onLeave: function (index, nextindex, direction) {
+			if (index ==2 && nextindex == 3) {
+				$('.sofa-hide').addClass('animate').on('animationend',function(){
+					// 当该动画执行完后触发该事件
+					$('.screen03 .size img:last-child').show();
+					$('.screen03 .btns img:last-child').show();
+				})
+			} else if (index == 3 && nextindex == 4) {
+				$(".screen04 .cloud").addClass('animate');
+				// 第三屏进入第四屏 动画
+				$('.screen03 .hide-sofa').addClass('animate').on('animationend',function(){
+					$('.screen04 .cart img:first-child').show();
+					$('.screen04 .cart').addClass('animate').on('transitionend', function () {
+						$(".screen04 .text img:last-child").fadeIn(500, function () {
+							$(".screen04 .address").fadeIn(500, function () {
+								$(".screen04 .address img:last-child").fadeIn();
+							});
+						});
+					})
+				})
+				
+			}
+		},
 		// 页面结构加载完成后触发该事件
 		afterRender: function () {}
 	});
